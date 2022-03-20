04940e266b8b14425b18d0ff8ec2a4ffec060519
Nadynn <91049087+Nadynn@users.noreply.github.com>
2022-03-20 16:34:43 +0200

    Нужно больше функций (#3)
    * Нужно больше функций
    
    * Нужно больше функций2
    
    * Нужно больше функций3

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+getRandomPositiveInteger();
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+checkStringLength('строка', 5);
