41f9fa724fd952d58fda4997fdea77478d83860e
Keks <keks@htmlacademy.ru>
2022-04-10 13:31:36 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/util.js
@@ -0,0 +1,23 @@
+//Генерация числа из массива
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+// Функция для проверки максимальной длины строки.
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+// Функция закрытия окна по Escape
+const isEscapeEvent = (evt, action) => {
+  if (evt.key === 'Escape') {
+    action();
+  }
+};
+
+export {getRandomPositiveInteger};
+export {checkStringLength};
+export {isEscapeEvent};
