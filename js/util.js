35cb09bd93fd2eb5792971c675901a5696c0f45b
Nadynn <91049087+Nadynn@users.noreply.github.com>
2022-04-17 16:55:56 +0300

    Надо подкачаться (#12)
    * Надо подкачаться1
    
    * Надо подкачаться 2

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 //Генерация числа из массива
 function getRandomPositiveInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
@@ -18,6 +20,26 @@ const isEscapeEvent = (evt, action) => {
   }
 };
 
-export {getRandomPositiveInteger};
-export {checkStringLength};
-export {isEscapeEvent};
+//Показ сообщения об отправке с ошибкой на 5 секунд
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {getRandomPositiveInteger, checkStringLength, isEscapeEvent, showAlert};
--- js/validation.js
+++ js/validation.js
@@ -1,4 +1,6 @@
-import {checkStringLength} from './util.js';
+import {checkStringLength, showAlert} from './util.js';
+import {sendData} from './api.js';
+import {onSuccessCloseForm, onErrorCloseForm, blockSubmitButton, unblockSubmitButton} from './form.js';
 
 // Поиск классов для ввода текста и комментариев
 const uploadForm = document.querySelector('.img-upload__form');
@@ -13,22 +15,23 @@ const CommentsFeatures = {
   MAX_LENGTH: 140
 };
 
-const HashtagsFeatures = {
+const hashtagsFeatures = {
   MAX_NUMBER: 5,
   MAX: 20,
   REGULAR: /^#[A-Za-za-Яа-яЁё 0-9]{1,19}$/,
   IS_HASH_SYMBOL: /[^#]/,
+  IS_SPACE: /.#/g,
 };
 
 //Сообщания об ошибках
-const ErrorMessages = {
+const errorMessages = {
   COMMENT_LONG: `Комментарий не может составлять больше ${CommentsFeatures.MAX_LENGTH} символов`,
   SPACE_HASHTAGS: 'Хэш-теги должны разделяться пробелами',
   HASH_SYMBOL: 'Хэш-тег должен начинаться с символа # (решётка)',
   TOO_SHORT: 'Хэш-тег не может состоять только из символа # (решётка)',
-  HASHTAGS_LONG: `Максимальная длина одно хэш-тега не должна превышать ${HashtagsFeatures.MAX} символов`,
+  HASHTAGS_LONG: `Максимальная длина одно хэш-тега не должна превышать ${hashtagsFeatures.MAX} символов`,
   UNIQUE: 'Хэш-тег не может быть использован дважды',
-  OVER_MAX: `Количество хэш-тегов не должно быть больше ${HashtagsFeatures.MAX_NUMBER}`,
+  OVER_MAX: `Количество хэш-тегов не должно быть больше ${hashtagsFeatures.MAX_NUMBER}`,
   BAG_SYMBOL_MESSAGE: 'Строка после решетки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и.т.п), символы пунктуации (тире, запятая, и.т.п), эмодзи'
 };
 
@@ -41,11 +44,29 @@ const pristine = new Pristine(uploadForm, {
   errorTextClass: 'text'
 });
 
-//Обработчик отправки
-uploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
+//Обработчик отправки формы
+const setUserFormSubmit = (onSuccess) => {
+  uploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+    if (isValid) {
+      blockSubmitButton();
+      sendData(
+        () => {
+          onSuccess();
+          onSuccessCloseForm();
+          unblockSubmitButton();
+        },
+        () => {
+          showAlert('Не удалось отправить форму. Попробуйте ещё раз');
+          onErrorCloseForm();
+          unblockSubmitButton();
+        },
+        new FormData(evt.target),
+      );
+    }
+  });
+};
 
 //Функция деления строки хэштегов по указаннаму элементу separator
 const stringToArray = (string, separator) => string.split(separator);
@@ -54,33 +75,34 @@ const stringToArray = (string, separator) => string.split(separator);
 const getCommentTextareaInput = (value) => (
   checkStringLength(value, CommentsFeatures.MAX_LENGTH)
 );
-pristine.addValidator(commentTextarea, getCommentTextareaInput, ErrorMessages.COMMENT_LONG);
+pristine.addValidator(commentTextarea, getCommentTextareaInput, errorMessages.COMMENT_LONG);
 
 // Обработчик проверки длины строки ввода хештега (не более 20 символов)
 pristine.addValidator(inputHashtags, () => {
   const hashtags = stringToArray(inputHashtags.value.toLowerCase(), SPACE_HASHTAG_SEPARATOR);
-  return hashtags.every((hashtag) => hashtag.length <= HashtagsFeatures.MAX);
-}, ErrorMessages.HASHTAGS_LONG);
+  return hashtags.every((hashtag) => hashtag.length <= hashtagsFeatures.MAX);
+}, errorMessages.HASHTAGS_LONG);
 
-//Проверка ввода недопустимых регулярных символов
+//Проверка ввода недопустимых регулярных символов----
 pristine.addValidator(inputHashtags, () => {
   const hashtags = stringToArray(inputHashtags.value.toLowerCase(), SPACE_HASHTAG_SEPARATOR);
-  return hashtags.every((hashtag) => HashtagsFeatures.REGULAR.test(hashtag));
-}, ErrorMessages.BAG_SYMBOL_MESSAGE);
+  return hashtags.every((hashtag) => (hashtagsFeatures.REGULAR.test(hashtag)) || (hashtag.length === 0));
+}, errorMessages.BAG_SYMBOL_MESSAGE);
 
-//Проверка на обязательное наличие первого символа '#'
+//Проверка на обязательное наличие первого символа '#'----
 pristine.addValidator(inputHashtags, () => {
   const hashtags = stringToArray(inputHashtags.value.toLowerCase(), SPACE_HASHTAG_SEPARATOR);
-  return hashtags.every((hashtag) => hashtag.startsWith('#'));
-}, ErrorMessages.HASH_SYMBOL);
+  return hashtags.every((hashtag) => (hashtag.startsWith('#')) || (hashtag.length === 0));
+}, errorMessages.HASH_SYMBOL);
 
-//Проверка на наличие ввода только символа #
+//Проверка на наличие ввода только символа '#'----
 pristine.addValidator(inputHashtags, (value) => {
-  if (value.match(HashtagsFeatures.IS_HASH_SYMBOL)) {
+  if (value.match(hashtagsFeatures.IS_HASH_SYMBOL) || value.length === 0) {
     return true;
   }
   return false;
-}, ErrorMessages.TOO_SHORT);
+}, errorMessages.TOO_SHORT);
+
 
 //Проверка наличия повторного хэштега
 pristine.addValidator(inputHashtags, () => {
@@ -95,20 +117,20 @@ pristine.addValidator(inputHashtags, () => {
     }
   }
   return duplicateExists;
-}, ErrorMessages.UNIQUE);
+}, errorMessages.UNIQUE);
 
 //Проверка количества введенных хештегов не более 5
 pristine.addValidator(inputHashtags, (value) => {
-  if (!(value.split(' ').length > HashtagsFeatures.MAX_NUMBER)) {
+  if (!(value.split(' ').length > hashtagsFeatures.MAX_NUMBER)) {
     return true;
   }
   return false;
-}, ErrorMessages.OVER_MAX);
+}, errorMessages.OVER_MAX);
 
-//Проверка пробелов между хэштегами
-pristine.addValidator(inputHashtags, (value) => {
-  if (value.split('#').length - 1 > 1) {
-    return true;
-  }
-  return false;
-}, ErrorMessages.SPACE_HASHTAGS);
+//Проверка пробелов между хэштегами---
+pristine.addValidator(inputHashtags, () => {
+  const hashtags = stringToArray(inputHashtags.value.toLowerCase(), SPACE_HASHTAG_SEPARATOR);
+  return hashtags.every((hashtag) => (hashtagsFeatures.REGULAR.test(hashtag) || hashtag.length === 0));
+}, errorMessages.SPACE_HASHTAGS);
+
+export {setUserFormSubmit};
